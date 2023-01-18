b402ceb98eef57b8f4e5a9ef3f23930ce073ebc5
Anastasiya-Lagutkina <109916879+Anastasiya-Lagutkina@users.noreply.github.com>
2023-01-18 17:34:09 +0300

    Add files via upload

---
+++ Program9(2).pas
@@ -0,0 +1,18 @@
+var a:array[1..10] of integer;
+    i,k,N:integer;
+begin
+write('¬ведите N:');
+readln(N);
+write('¬ведите элементы массива:');
+for i:=1 to N do 
+read (A[i]);
+
+k:=0;
+for i:=1 to N-1 do
+if abs(A[i]+A[i+1])=abs(A[i]+abs(A[i+1]))then
+begin
+k:=i;
+break;
+end;
+writeln(k);
+end.
