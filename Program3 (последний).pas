313182fd7f9ca2a9a9f5481b1abf0667642d8991
Anastasiya-Lagutkina <109916879+Anastasiya-Lagutkina@users.noreply.github.com>
2023-01-18 17:29:09 +0300

    Add files via upload

---
+++ Program3 (последний).pas
@@ -0,0 +1,22 @@
+program ml;
+var
+ a:array [1..10] of integer;
+ i:integer;
+ k:integer;
+begin
+readln(a[1]);
+readln(a[10]);
+randomize;
+for i:=2 to 9 do
+a[i]:=random(10);
+for i:=1 to 10 do
+write(a[i]:3);
+writeln;
+k:=10;
+for i:=2 to 9 do
+if (a[i]>a[1]) and (a[i]<a[10]) then
+k:=i;
+if k<=9 then
+writeln('k=',k)
+else writeln('Таких элементов нет')
+end.
